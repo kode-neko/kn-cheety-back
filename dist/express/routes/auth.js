77f8335ad322a4547ad604268349225ed29de68f
Kodeneko <ladysun.freedom@gmail.com>
2022-09-27 00:43:38 +0200

    [dev] index select env

--- dist/express/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controller/auth");
-const router = (0, express_1.Router)();
-router.post('/', auth_1.signup);
-router.put('/', auth_1.login);
-router.put('/:', auth_1.logout);
-exports.default = router;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import dotenv from 'dotenv';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import app from './server';
+import { console } from '../utils';
 
-dotenv.config();
-
-app.listen(process.env.SERVER_PORT);
+const argsv = yargs(hideBin(process.argv))
+  .option('m', {
+    alias: 'mode',
+    default: 'dev',
+    demandOption: true,
+    choices: ['dev', 'prod'],
+    describe: 'Select deploy mode',
+    type: 'string',
+  })
+  .argv as { [x: string]: unknown };
+console.log(argsv.m);
+switch (argsv.m) {
+  case 'prod':
+    dotenv.config();
+    console.log('Prod');
+    app.listen(process.env.SERVER_PORT);
+    break;
+  case 'dev':
+    dotenv.config({ path: '../.env.test' });
+    console.log('Dev');
+    app.listen(process.env.SERVER_PORT);
+    break;
+  default:
+    console.log('Parametro "mode" invalido');
+}
